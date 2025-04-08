f5b2b1a59c9b2783067e31497d62a699bfc35d9c
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-04-08 10:05:45 +0200

    theme folders

--- functions.php
+++ functions.php
@@ -26,12 +26,7 @@ function dino_theme() {
 
 }
 
-function dino_theme_remove_gutenberg() {
-    add_filter('use_block_editor_for_post', '__return_false', 10);
-    add_filter('use_block_editor_for_post_type', '__return_false', 10);
-}
-add_action('init', 'dino_theme_remove_gutenberg');
-add_action("wp_enqueue_scripts", "dino_theme");
+
 
 
 
