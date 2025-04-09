4e0d2fae04f173e42d772c6882a7a71ca17ed825
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-04-09 16:39:33 +0200

    navbar and script.js

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  });
