4414315104bec7cba5ad84c408f1eef2973b3bd5
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-04-09 16:44:13 +0200

    js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.querySelector(".navbar");
 
     window.addEventListener("scroll", function () {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 0) {
         navbar.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
