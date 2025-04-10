cb01bfa2f926acedfdfaafecdb7b8367ba625cae
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-04-10 11:11:11 +0200

    hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,14 +1,19 @@
+<?php 
+$hero_video = get_field("hero_video"); // ACF video file (mp4)
+?>
+
 <!-- Hero Section -->
 <section id="hero" class="hero-section position-relative vh-100 overflow-hidden">
-    <div class="hero-overlay position-absolute top-0 start-0 w-100 h-100"></div>
 
-    <!-- Temporarily Commenting out the Video Background -->
-    <!-- 
-    <?php if ($hero_video): ?>
-      <video autoplay muted loop playsinline class="hero-video w-100 h-100 position-absolute top-0 start-0 object-fit-cover">
-        <source src="<?php echo esc_url($hero_video); ?>" type="video/mp4">
-        Your browser does not support the video tag.
-      </video>
-    <?php endif; ?>
-    -->
+  <!-- Video Background -->
+  <?php if ($hero_video): ?>
+    <video autoplay muted loop playsinline class="hero-video w-100 h-100 position-absolute top-0 start-0 object-fit-cover">
+      <source src="<?php echo esc_url($hero_video); ?>" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  <?php endif; ?>
+
+  <!-- Overlay Filter -->
+  <div class="hero-overlay position-absolute top-0 start-0 w-100 h-100"></div>
+
 </section>
