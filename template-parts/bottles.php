6c3667b99c06ed3afe0a512ac1f2d6e798beb179
krihor176 <krihor01@easv365.dk>
2025-04-15 20:48:22 +0200

    fix 4

--- template-parts/bottles.php
+++ template-parts/bottles.php
@@ -12,12 +12,17 @@ $right_bottle_description = get_field('right_bottle_description');
 
 <section class="bottle-section">
   <div class="bottle-container">
-   <div class="bottle left-bottle" data-side="left">
-    <img src="<?php echo esc_url($left_bottle_img['url']); ?>" alt="<?php echo esc_attr($about_image['alt']); ?>">
-    </div> 
 
-    <div class="bottle right-bottle" data-side="right">
-    <img src="<?php echo esc_url($right_bottle_img['url']); ?>" alt="<?php echo esc_attr($about_image['alt']); ?>">
+    <div class="side left-side">
+      <div class="bottle left-bottle" data-side="left">
+        <img src="<?php echo esc_url($left_bottle_img['url']); ?>" alt="">
+      </div>
+    </div>
+
+    <div class="side right-side">
+      <div class="bottle right-bottle" data-side="right">
+        <img src="<?php echo esc_url($right_bottle_img['url']); ?>" alt="">
+      </div>
     </div>
 
     <div class="bottle-text left-text">
@@ -27,11 +32,8 @@ $right_bottle_description = get_field('right_bottle_description');
 
     <div class="bottle-text right-text">
       <h2><?php the_field('right_bottle_title'); ?></h2>
-      <p><?php the_field('left_bottle_description'); ?></p>
+      <p><?php the_field('right_bottle_description'); ?></p>
     </div>
+
   </div>
 </section>
-
-<div class="custom-cursor">
-  <span class="cursor-text">Click to see more</span>
-</div>
