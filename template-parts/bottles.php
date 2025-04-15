888fed27b52a01ff6fc9ddbf0c48f5d4968939ee
krihor176 <krihor01@easv365.dk>
2025-04-15 21:30:57 +0200

    fix 4

--- template-parts/bottles.php
+++ template-parts/bottles.php
@@ -12,23 +12,27 @@ $right_bottle_description = get_field('right_bottle_description');
 
 <section class="bottle-section">
   <div class="bottle-container">
-   <div class="bottle left-bottle" data-side="left">
-    <img src="<?php echo esc_url($left_bottle_img['url']); ?>" alt="<?php echo esc_attr($about_image['alt']); ?>">
-    </div> 
 
+    <!-- Left Bottle -->
+    <div class="bottle left-bottle" data-side="left">
+      <img src="<?php echo esc_url($left_bottle_img['url']); ?>" alt="<?php echo esc_attr($left_bottle_img['alt']); ?>">
+      <div class="bottle-text left-text">
+        <h2><?php echo esc_html($left_bottle_title); ?></h2>
+        <p><?php echo esc_html($left_bottle_description); ?></p>
+      </div>
+      <p class="tap-instruction">Tap to see more</p>
+    </div>
+
+    <!-- Right Bottle -->
     <div class="bottle right-bottle" data-side="right">
-    <img src="<?php echo esc_url($right_bottle_img['url']); ?>" alt="<?php echo esc_attr($about_image['alt']); ?>">
+      <img src="<?php echo esc_url($right_bottle_img['url']); ?>" alt="<?php echo esc_attr($right_bottle_img['alt']); ?>">
+      <div class="bottle-text right-text">
+        <h2><?php echo esc_html($right_bottle_title); ?></h2>
+        <p><?php echo esc_html($right_bottle_description); ?></p>
+      </div>
+      <p class="tap-instruction">Tap to see more</p>
     </div>
 
-    <div class="bottle-text left-text">
-      <h2><?php the_field('left_bottle_title'); ?></h2>
-      <p><?php the_field('left_bottle_description'); ?></p>
-    </div>
-
-    <div class="bottle-text right-text">
-      <h2><?php the_field('right_bottle_title'); ?></h2>
-      <p><?php the_field('left_bottle_description'); ?></p>
-    </div>
   </div>
 </section>
 
