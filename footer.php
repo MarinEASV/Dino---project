13a03b916490aa8d7ce862664d48deb5c704ce6a
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-04-17 11:57:07 +0200

    footer and hero

--- footer.php
+++ footer.php
@@ -29,24 +29,13 @@
             </div>
         </div>
         <div class="col-md-6 col-lg-6 col-xl-3">
-        <div class="footer-item d-flex flex-column">
-          <h4 class="mb-4" style="color: var(--c-dark);">Åbningstider</h4>
-          
-          <div class="opening-hours">
-            <div class="row"><span>Monday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Tuesday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Wednesday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Thursday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Friday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Saturday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Sunday</span><span>17:00 – 21:00</span></div>
-            <div class="row"><span>Holiday X</span><span>closed</span></div>
-            <div class="row"><span>Holiday X</span><span>closed</span></div>
+          <div class="footer-item d-flex flex-column">
+            <h4 class="mb-4" style="color: var(--c-dark);">Åbningstider</h4>
+            <p><i class="fas fa-angle-right me-2"></i>Monday</p>
+            <p><i class="fas fa-angle-right me-2"></i>Tuesday</p>
+            <p><i class="fas fa-angle-right me-2"></i>Wednesday</p>
           </div>
-
         </div>
-      </div>
-
         <div class="col-md-6 col-lg-6 col-xl-3">
           <div class="footer-item d-flex flex-column">
             <h4 class="mb-4" style="color: var(--c-dark);">Kontakt</h4>
