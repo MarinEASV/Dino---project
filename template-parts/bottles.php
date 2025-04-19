4f0bf97f5f3000d0aaa215efde1d044f3d6d885b
krihor176 <krihor01@easv365.dk>
2025-04-19 18:56:24 +0200

    mobile

--- template-parts/bottles.php
+++ template-parts/bottles.php
@@ -11,7 +11,7 @@ $right_bottle_description = get_field('right_bottle_description');
 ?>
 
 <section class="bottle-section">
-  <div class="bottle-container">
+  <div class="bottle-container" data-aos="fade-up">
    <div class="bottle left-bottle" data-side="left">
     <img src="<?php echo esc_url($left_bottle_img['url']); ?>" alt="<?php echo esc_attr($about_image['alt']); ?>">
     </div> 
