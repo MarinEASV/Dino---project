79647931d3c96eb1037f6fde72780d44a5098e6f
krihor176 <krihor01@easv365.dk>
2025-04-28 12:17:31 +0200

    small changes

--- footer.php
+++ footer.php
@@ -72,11 +72,11 @@
           </li>
           <li>
             <i class="fas fa-angle-right me-2"></i>
-            <a href="https://dinoproject.dk/wp-content/uploads/2025/04/Vilkaar_for_brug.docx" target="_blank" class="text-break">Terms of use</a>
+            <a href="https://dinoproject.dk/wp-content/uploads/2025/04/Vilkaar_for_brug.pdf" target="_blank" class="text-break">Terms of use</a>
           </li>
           <li>
             <i class="fas fa-angle-right me-2"></i>
-            <a href="https://dinoproject.dk/wp-content/uploads/2025/04/Privatlivspolitik.docx" target="_blank" class="text-break">Privacy policy</a>
+            <a href="https://dinoproject.dk/wp-content/uploads/2025/04/Privatlivspolitik.pdf" target="_blank" class="text-break">Privacy policy</a>
           </li>
         </ul>
       </div>
