6f148ec1a2d9b1ca2c316dc7a5b6fa088c86b5f5
krihor176 <krihor01@easv365.dk>
2025-04-30 11:21:20 +0200

    css changes footer

--- template-parts/reviews.php
+++ template-parts/reviews.php
@@ -8,19 +8,8 @@
 <section id="reviews-section" class="reviews-section">
   <div class="container">
     <h2 class="reviews-section__title">What Our Customers Are Saying</h2>
-
-  <?php
-      // WP Google Reviews shortcode.
-      // Change place_id / layout / columns as desired.
-      echo do_shortcode(
-        '[trustindex data-widget-id=9b751f44535237868c561900108]'
-      );
-      
+    <?php
+      echo do_shortcode( '[trustindex no-registration=google]' );
     ?>
-<?php
-echo do_shortcode(
-        '[trustindex no-registration=google]'
-      );
-?>
   </div>
 </section>
