5021cd036f28f06b0737a6581cb3a1d5d0811140
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2025-05-10 10:04:36 +0200

    fixes

--- template-parts/reviews.php
+++ template-parts/reviews.php
@@ -5,7 +5,7 @@
  */
 ?>
 
-<section id="reviews-section" class="reviews-section" data-aos="fade-up">
+<section id="reviews-section" class="reviews-section">
   <div class="container">
     <h2 class="reviews-section__title">Hvad vores kunder siger</h2>
 
