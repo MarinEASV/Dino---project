53b950a9c20c95889c972bbbb81271f5ff7433c8
krihor176 <krihor01@easv365.dk>
2025-05-12 11:17:01 +0200

    language switcher

--- functions.php
+++ functions.php
@@ -1,10 +1,28 @@
 <?php
 add_action( 'init', function() {
     if ( function_exists( 'pll_register_string' ) ) {
-        pll_register_string( 'Cursor: open label',  'Click to see more' );
-        pll_register_string( 'Cursor: close label', 'Close' );
+
+        pll_register_string( 'Cursor open label',  'Click to see more' );
+        pll_register_string( 'Cursor close label', 'Close' );
+
+        pll_register_string( 'Menu link',    'Menu' );
+        pll_register_string( 'About link',   'Om os' );
+        pll_register_string( 'Contact link', 'Kontakt' );
+        pll_register_string( 'Reserve btn',  'Reserve' );
+
+        pll_register_string( 'Phone',                        '📞 +45 75 16 64 64' );
+        pll_register_string( 'Reservation info',             'Reservationsinfo' );
+        pll_register_string( 'Reservation info text',        'Vi håndterer kun bordreservationer og madbestillinger via telefonopkald.' );
+
+
+        pll_register_string( 'Taste of Fanø',                'D I N   S M A G   A F   F A N Ø' );
+
+        pll_register_string( 'Authentic Balkan & Danish Cuisine', 'Autentisk Balkan & Dansk Køkken' );
+        pll_register_string( 'Our menu',                      'Vores menu' );
+        pll_register_string( 'Menu description',              'Traditionelle danske retter & Balkan favoritter' );
     }
 });
+
 
 function dino_theme_enqueue_assets() {
     // Styles
@@ -27,4 +45,9 @@ add_action('wp_enqueue_scripts', 'dino_theme_enqueue_assets');
 
 // Optional: Disable Gutenberg
 function dino_theme_remove_gutenberg() {
-    remove_post_type_support("
+    remove_post_type_support("post", "editor");
+    remove_post_type_support("page", "editor");
+}
+add_action("init", "dino_theme_remove_gutenberg");
+add_filter("use_block_editor_for_post", "__return_false");
+add_filter("use_block_editor_for_page", "__return_false");
