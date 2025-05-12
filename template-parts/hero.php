f99b534f5c19e99d5131f4319f55c524eff2a277
krihor176 <krihor01@easv365.dk>
2025-05-12 11:48:52 +0200

    almost done

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -30,8 +30,4 @@ $hero_video = get_field("hero_video"); // ACF video file (mp4)
   <!-- Play ➔ scroll to #video -->
   <a href="#video" class="scroll-to-video" aria-label="Watch Video">
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <polygon points="9,7 9,17 17,12"/>
-    </svg>
-  </a>
-
-</section>
+      <polygon points="9,7 9,
